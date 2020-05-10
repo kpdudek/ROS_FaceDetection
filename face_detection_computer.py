2ad16dfc69135ce3bd770368486228d736de74cc
Kurt <kpdudek2@gmail.com>
2020-05-10 12:06:32 -0400

    Backing up

--- face_detection_computer.py
+++ face_detection_computer.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python
+
 import rospy
 import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-#from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import RegionOfInterest
-from geometry_msgs.msg import PoseStamped
 
 # Define global variables
 global pub_Image
@@ -48,53 +48,7 @@ def callback(image_msg):
     # Publish the original image with the rectangles
     mod_img = bridge.cv2_to_imgmsg(cv_img, encoding="bgr8")
     pub_Image.publish(mod_img)
-    
-    #Follow face
-    pose_msg = PoseStamped()
-  
-    fig_x = image_msg.width/2
-    fig_y = image_msg.height/2
-    #kx = .001
-    #ky = .001
-    kx = .003
-    ky = .003
-    #xmax_m = 0.70
-    #xmin_m = -1.0
-    xmax_m = -3.5
-    xmin_m = -5.5
-    zmin_m = 0.5;
-    zmax_m = 1.5;
-    
-    if len(faces) > 0:
-        face_x = boxes_msg.x_offset + (boxes_msg.width/2)
-        face_y = boxes_msg.y_offset + (boxes_msg.height/2)
-        x_error = fig_x - face_x
-        y_error = fig_y - face_y
-        pose_msg.pose.position.x = current_pose.pose.position.x + (x_error*kx)
-        pose_msg.pose.position.z = current_pose.pose.position.z + (y_error*ky)
-    else:
-	# No face to follow, remain stationary
-        pose_msg = goal_pose
-    if pose_msg.pose.position.x > xmax_m:
-        pose_msg.pose.position.x = xmax_m
-    if pose_msg.pose.position.x < xmin_m:
-	pose_msg.pose.position.x = xmin_m
-    if pose_msg.pose.position.z > zmax_m:
-	pose_msg.pose.position.z = zmax_m
-    if pose_msg.pose.position.z < zmin_m:
-	pose_msg.pose.position.z = zmin_m
 
-    #pose_msg.pose.position.x = goal_pose.pose.position.x
-    pose_msg.pose.position.y = goal_pose.pose.position.y
-    pose_msg.pose.orientation = goal_pose.pose.orientation
-    local_position_pub.publish(goal_pose)
-
-    # Update goal pose to be pose_msg
-    goal_pose = pose_msg
-
-def pos_sub_callback(pose_sub_data):
-    global current_pose
-    current_pose = pose_sub_data
     
 def main():
     global pub_Image
@@ -117,18 +71,6 @@ def main():
     pub_BoxesImage = rospy.Publisher('/face_detection/boxes_image', Image, queue_size=1)
     pub_Boxes = rospy.Publisher('/face_detection/boxes', RegionOfInterest, queue_size=1)
     rospy.Subscriber('/raspicam_node/image', Image, callback, queue_size=1, buff_size=2**18)
-    local_position_subscribe = rospy.Subscriber('/mavros/mocap/pose', PoseStamped, pos_sub_callback)
-    local_position_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size = 1)
-    current_pose = PoseStamped()
-    goal_pose = PoseStamped()
-    # Set default waypoint
-    goal_pose.pose.position.x = -4.64
-    goal_pose.pose.position.y = -1.33
-    goal_pose.pose.position.z = 0.7
-    goal_pose.pose.orientation.x = 0
-    goal_pose.pose.orientation.z = -.7071
-    goal_pose.pose.orientation.y = 0
-    goal_pose.pose.orientation.w = .707
 
     # Keep program alive until we stop it
     rospy.spin()
